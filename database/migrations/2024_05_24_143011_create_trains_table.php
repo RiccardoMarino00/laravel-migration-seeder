9166f04cbdc74fd6aa974c9555b6a77c0ed7bcac
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-24 16:44:39 +0200

    Added: trains table

---
+++ database/migrations/2024_05_24_143011_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->time('orario_di_partenza', '');
+            $table->time('orario_di_arrivo', '');
+            $table->string('codice_treno', 100);
+            $table->bigInteger('numero_carrozze');
+            $table->tinyInteger('in_orario');
+            $table->tinyInteger('cancellato');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
